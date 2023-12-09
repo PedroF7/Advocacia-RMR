2192d9d2037d7ee7344c4245697fdbf4e2acfdc8
PedroF7 <pedrofreitassantos2021@gmail.com>
2023-12-09 14:43:15 -0300

    js

--- js/reveal.js
+++ js/reveal.js
@@ -43,41 +43,43 @@ revelar.reveal('.efeito-pessoas-bpc'),
     origin: 'top'
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Links a serem sorteados
-    const links = [
-      "https://api.whatsapp.com/send?phone=5521988604191",
-      "https://api.whatsapp.com/send?phone=5521997401505",
-      "https://api.whatsapp.com/send?phone=5521998705309",
-    ];
+//   document.addEventListener('DOMContentLoaded', function() {
+//     // Links a serem sorteados
+//     const links = [
+//         "https://api.whatsapp.com/send?phone=5521988604191",
+//         "https://api.whatsapp.com/send?phone=5521997401505",
+//         "https://api.whatsapp.com/send?phone=5521998705309",
+//     ];
 
-    // Variável para armazenar o link mais acessado
-    let linkMaisAcessado = null;
+//     // Variável para armazenar o link mais acessado
+//     let linkMaisAcessado = null;
 
-    // Função para sortear um link
-    function sortearLink() {
-      const indexSorteado = Math.floor(Math.random() * links.length);
-      return links[indexSorteado];
-    }
+//     // Função para sortear um link
+//     function sortearLink() {
+//       const indexSorteado = Math.floor(Math.random() * links.length);
+//       return links[indexSorteado];
+//     }
 
-    function redirecionar() {
-      if (linkMaisAcessado) {
-        window.location.href = linkMaisAcessado;
-      }
-    }
+//     // Função para redirecionar para o link mais acessado
+//     function redirecionar() {
+//       if (linkMaisAcessado) {
+//         window.location.href = linkMaisAcessado;
+//       }
+//     }
 
-    document.querySelector('.container').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
+//     // Adiciona eventos de clique nas classes "container" e "whats"
+//     document.querySelector('.container').addEventListener('click', function() {
+//       const linkSorteado = sortearLink();
+//       linkMaisAcessado = linkSorteado;
+//       window.open(linkSorteado, '_blank');
+//     });
 
-    document.querySelector('.whats').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
+//     document.querySelector('.whats').addEventListener('click', function() {
+//       const linkSorteado = sortearLink();
+//       linkMaisAcessado = linkSorteado;
+//       window.open(linkSorteado, '_blank');
+//     });
 
-    // Adiciona evento de redirecionamento quando a página for fechada
-    window.addEventListener('beforeunload', redirecionar);
-  });
+//     // Adiciona evento de redirecionamento quando a página for fechada
+//     window.addEventListener('beforeunload', redirecionar);
+//   });
