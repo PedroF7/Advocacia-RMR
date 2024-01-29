3cc3dadfa71445fa85ddf71bc6b36e79f1c32cc0
Pedro Freitas <pedrofreitassantos2021@gmail.com>
2024-01-28 21:34:16 -0300

    Update linkswpp.js

--- js/linkswpp.js
+++ js/linkswpp.js
@@ -1,73 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Links a serem sorteados
-    const links = [
-      "https:api.whatsapp.com/send/?phone=5521998705309&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0",
+  // Links a serem sorteados
+  const links = [
+    "https://api.whatsapp.com/send/?phone=5521998705309&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0",
 
-      "https:api.whatsapp.com/send/?phone=5521997401505&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0",
+    "https://api.whatsapp.com/send/?phone=5521997401505&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0",
 
-      "https:api.whatsapp.com/send/?phone=5521988604191&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0"
-    ];
-    
+    "https://api.whatsapp.com/send/?phone=5521988604191&text=Quero+saber+se+tenho+direito+ao+bpc-loas&type=phone_number&app_absent=0"
+  ];
+  
 
-    // Variável para armazenar o link mais acessado
-    let linkMaisAcessado = null;
+  // Variável para armazenar o link mais acessado
+  let linkMaisAcessado = null;
 
-    // Função para sortear um link
-    function sortearLink() {
-      const indexSorteado = Math.floor(Math.random() * links.length);
-      return links[indexSorteado];
+  // Função para sortear um link
+  function sortearLink() {
+    const indexSorteado = Math.floor(Math.random() * links.length);
+    return links[indexSorteado];
+  }
+  
+  function redirecionar() {
+    if (linkMaisAcessado) {
+      window.location.href = linkMaisAcessado;
     }
+  }
 
-    // Função para redirecionar para o link mais acessado
-    function redirecionar() {
-      if (linkMaisAcessado) {
-        window.location.href = linkMaisAcessado;
-      }
-    }
-
-    // Adiciona eventos de clique nas classes "container" e "whats"
-
-    document.querySelector('.btn-ajuda').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.whats').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.btn-link').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.btn-direito').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.btn-bpc').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.btn-bpc-ult').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-
-    document.querySelector('.btn-especialistas').addEventListener('click', function() {
-      const linkSorteado = sortearLink();
-      linkMaisAcessado = linkSorteado;
-      window.open(linkSorteado, '_blank');
-    });
-    // Adiciona evento de redirecionamento quando a página for fechada
-    window.addEventListener('beforeunload', redirecionar);
+  document.querySelector('.btn-ajuda').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
   });
+
+  document.querySelector('.whats').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+
+  document.querySelector('.btn-link').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+
+  document.querySelector('.btn-direito').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+
+  document.querySelector('.btn-bpc').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+
+  document.querySelector('.btn-bpc-ult').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+
+  document.querySelector('.btn-especialistas').addEventListener('click', function() {
+    const linkSorteado = sortearLink();
+    linkMaisAcessado = linkSorteado;
+    window.open(linkSorteado, '_blank');
+  });
+  window.addEventListener('beforeunload', redirecionar);
+});
